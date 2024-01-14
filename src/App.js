08ec68b36c7fde42530ef7d1adfd7847cac98f8b
KeplerInCoding <shreyakv8@gmail.com>
2024-01-15 01:06:56 +0530

    image setup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import ExamDetails from "./pages/ExamDetails";
+import add from "./assets/images/Ads.png"
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <div>
+        <img width="980px" src={add} alt=""/>
+      </div>
 
       <Routes>
         <Route path="/" element={<Home />} />
