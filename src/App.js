18d8d79fbcfabe9970ac1d6bf53c6f693848f385
KeplerInCoding <shreyakv8@gmail.com>
2024-01-15 00:27:00 +0530

    router set up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import "./App.css";
+import {Route, Routes} from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import ExamDetails from "./pages/ExamDetails";
 
 function App() {
   return (
-    <div>hello world</div>
+    <div>
+      <Navbar/>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/examDetails" element={<ExamDetails />} />
+      </Routes>
+
+      <Footer/>
+
+    </div>
   );
 }
 
