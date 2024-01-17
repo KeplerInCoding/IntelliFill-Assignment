576f39c78089d0c350c4f6445ae78d8e8c054e0a
KeplerInCoding <shreyakv8@gmail.com>
2024-01-17 18:59:56 +0530

    useNAvigate on back

--- src/pages/ExamDetails.js
+++ src/pages/ExamDetails.js
@@ -1,17 +1,24 @@
-import React from 'react'
-import JeeCard from '../components/JeeCard'
-import Description from '../components/Description'
-import Interested from '../components/Interested'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import JeeCard from '../components/JeeCard';
+import Description from '../components/Description';
+import Interested from '../components/Interested';
 
 const ExamDetails = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div className='w-screen flex justify-center'>
       <div className='md:w-5/6 w-screen p-5'>
-        <div className='flex gap-5 items-center font-bold'>
-          <i class="fa-solid fa-arrow-left"></i>
+        <div className='flex gap-5 items-center font-bold' onClick={handleGoBack} style={{ cursor: 'pointer' }}>
+          <i className="fa-solid fa-arrow-left"></i>
           <div className='text-lg my-3'>Back</div>
         </div>
-        <div className='flex'>
+        <div className='flex flex-wrap'>
           <div>
             <JeeCard/>
             <Description/>
@@ -20,7 +27,7 @@ const ExamDetails = () => {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ExamDetails
+export default ExamDetails;
